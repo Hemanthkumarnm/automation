8d003899630aa4617ac5f57ff8d1a3e4b0eeeb4c
Hemanth kumar <hemanth.drivex@drivex.in>
2025-01-17 11:20:26 +0530

    commands related to git local to remote

--- first.py
+++ first.py
@@ -1,1 +1,17 @@
 This file is for test case on github from local 
+
+ 1119  git config --global user.name "Hemanth kumar"
+ 1120  git conifg --global --list
+ 1121  gith --help
+ 1122  git --help
+ 1123  clear
+ 1124  history
+ 1125  touch first.py
+ 1126  ls
+ 1127  vi first.py
+ 1128  git status
+ 1129  git add .
+ 1130  git status
+ 1131  git commit -m "given a test case line"
+ 1132  git status 
+ 1133  git push -u origin main
